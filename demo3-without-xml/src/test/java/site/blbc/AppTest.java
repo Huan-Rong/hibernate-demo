fe759e84ba41798531ee8058adac48235384a77f
Huan-Rong <huanrong.maybe@gmail.com>
2019-07-29 14:41:19 +0800

    1.Add a new module: demo3-without-xml

--- demo3-without-xml/src/test/java/site/blbc/AppTest.java
+++ demo3-without-xml/src/test/java/site/blbc/AppTest.java
@@ -1,23 +1,20 @@
 package site.blbc;
 
-import static org.junit.Assert.assertTrue;
 
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataBuilder;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import site.blbc.entity.SysAccountEntity;
 
-import java.time.LocalDate;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Unit test for simple App.
@@ -29,13 +26,25 @@ public class AppTest {
 
     @BeforeClass
     public static void setUp() {
-        final StandardServiceRegistry registry =
-                new StandardServiceRegistryBuilder().configure().build();
+        Properties properties = new Properties();
+        properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/test?useSSL=false");
+        properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
+        properties.setProperty(Environment.USER, "ian");
+        properties.setProperty(Environment.PASS, "ian");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL57Dialect");
+        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
+
         try {
-            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration.setProperties(properties);
+
+            configuration.addAnnotatedClass(SysAccountEntity.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } catch (Exception e) {
             log.error("创建 SessionFactory 失败！", e);
-            StandardServiceRegistryBuilder.destroy(registry);
         }
     }
 
@@ -44,18 +53,6 @@ public class AppTest {
         if (sessionFactory != null) {
             sessionFactory.close();
         }
-    }
-
-    @Test
-    public void saveTest() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        SysAccountEntity sysAccountEntity = SysAccountEntity.builder()
-                .name("test").username("test").password("test").birthday(LocalDate.now()).build();
-        Long id = (Long) session.save(sysAccountEntity);
-        session.getTransaction().commit();
-        session.close();
-        Assert.assertNotNull(id);
     }
 
     @Test
